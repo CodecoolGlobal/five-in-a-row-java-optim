dc5feff101892a2d4abb0c2a7351cbfe22bfa735
Adrian-P0gar <pogar.adi@gmail.com>
2020-05-07 20:04:50 +0300

    game mechanics, with out condition for win

--- src/main/java/com/codecool/fiveinarow/FiveInARow.java
+++ src/main/java/com/codecool/fiveinarow/FiveInARow.java
@@ -1,11 +1,88 @@
 package com.codecool.fiveinarow;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.codecool.fiveinarow.Game;
 public class FiveInARow {
 
-    public static void main(String[] args) {
-        Game game = new Game(11, 11);
-        game.enableAi(1);
-        game.enableAi(2);
-        game.play(5);
+    public static void main(String[] args) throws IOException {
+
+
+        firstMenu();
+        int [] boardsOptions = getFirstInput();
+        Game game1 = new Game(boardsOptions[0], boardsOptions[1]);
+        while (true){
+        int player = 8;
+        while(player == 8){
+            System.out.println("Player 8 choose: ");
+            game1.printBoard();
+            int[] playerMove = game1.getAiMove(8);
+            if(game1.mark(playerMove[0],playerMove[1],playerMove[2]) == true){
+
+            player = 9;}
+            else
+            {
+                System.out.println("Player 8, this position is occupied, please choose a empty one! ");
+
+            }
+
+        }
+        while(player == 9){
+            System.out.println("Player 9 choose: ");
+            game1.printBoard();
+            int[] playerMove = game1.getAiMove(9);
+            if(game1.mark(playerMove[0],playerMove[1],playerMove[2]) == true){
+                if(game1.isFull() == false){
+                player = 8;}
+                else {
+                    System.out.println("IS DRAW!!!");
+                    break;
+                }
+            }
+            else
+            {
+                System.out.println("Player 9, this position is occupied, please choose a empty one! ");
+
+            }
+
+        }}
+//        int[][] g2 = new int[][]{{0,0},{0,1}};
+//        game1.setBoard(g2);
+//      System.out.println(game1);
+//        game1.printBoard();
+//            int[] playerMove = game1.getAiMove(8);
+//            game1.mark(playerMove[0],playerMove[1],playerMove[2]);
+//        game1.printBoard();
+//            playerMove = game1.getAiMove(5);
+//
+//        game1.mark(playerMove[0],playerMove[1],playerMove[2]);
+//
+//        game1.printBoard();
+
+    }
+    public static int firstMenu(){
+        System.out.println("FIVE IN A ROW \n");
+        System.out.println("1. Play");
+        System.out.println("2. Quit");
+        Scanner userOption = new Scanner(System.in);
+        System.out.println("Choose: ");
+        int choose = userOption.nextInt();
+        return choose;
+
+    }
+    public static int[] getFirstInput(){
+        Scanner userRows = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("How many rows: ");
+
+        int rows = userRows.nextInt();  // Read user input
+
+        Scanner userColumns = new Scanner(System.in);
+        System.out.println("How many columns: ");
+        int cols = userColumns.nextInt();
+
+        int[] boardOptions = {rows, cols};
+        return boardOptions;
     }
 }
--- src/main/java/com/codecool/fiveinarow/Game.java
+++ src/main/java/com/codecool/fiveinarow/Game.java
@@ -1,40 +1,107 @@
 package com.codecool.fiveinarow;
-
-public class Game implements GameInterface {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+public class Game  {
 
     private int[][] board;
 
-    public Game(int nRows, int nCols) {
-    }
+
+
+    public Game(int nRows, int nCols ) {
+//        nRows = 2, n Cols =3;
+        this.board= new int[nRows][nCols];
+        for(int row= 0; row< nRows; row++){
+            for(int col =0; col<nCols; nCols++){
+                board[row][col] = 0;
+            }
+        }
+        }
+
+
+
+
+
 
     public int[][] getBoard() {
+
         return board;
     }
 
     public void setBoard(int[][] board) {
+
         this.board = board;
+
     }
 
     public int[] getMove(int player) {
+//        user input
+
         return null;
     }
 
     public int[] getAiMove(int player) {
-        return null;
+//        user input 2
+        Scanner userRow = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("\n"+"Enter row: ");
+
+        int row = userRow.nextInt();  // Read user input
+
+        Scanner userColumn = new Scanner(System.in);
+        System.out.println("\n" + "Enter column: ");
+        int col = userColumn.nextInt();
+        int playerMove[] = {player,row,col};
+        return playerMove;
     }
 
-    public void mark(int player, int row, int col) {
+    public boolean mark(int player, int row, int col) {
+        row = row-1;
+        col=col -1;
+        if(board[row][col] == 0){
+        board[row][col] = player;
+        return true;}
+        else{
+            return false;
+        }
     }
 
     public boolean hasWon(int player, int howMany) {
+//        win condition
         return false;
     }
 
     public boolean isFull() {
-        return false;
+//
+        for (int row =0; row < getLengthRow(board); row++){
+            for(int col=0; col < getLengthCol(board); col++){
+                if (board[row][col]== 0){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public void printBoard() {
+//        print board
+        int counterRowsNumbers = 1;
+        int rows = getLengthRow(board);
+        int columns = getLengthCol(board);
+        System.out.println("" + "\n");
+
+        System.out.print("_  ");
+        for(int i =0; i <columns; i++){
+            System.out.print(  i+1 +" | ");
+
+        }
+        for (int x = 0; x<rows; x++, counterRowsNumbers++ ){
+            System.out.print( "\n"+ counterRowsNumbers + "| ");
+            for(int y = 0; y<columns; y++){
+                System.out.print(  board[x][y]+ " | ");
+
+            }
+        }
     }
 
     public void printResult(int player) {
@@ -43,7 +110,20 @@ public class Game implements GameInterface {
     public void enableAi(int player) {
     }
 
+    @Override
+    public String toString() {
+        return "Game{" +
+                "board=" + Arrays.deepToString(this.board) + getLengthRow(this.board) + getLengthCol(this.board) +
+                '}';
+    }
+    public int getLengthRow(int [][] board){
+
+        return board.length;
+    }
+
+    public int getLengthCol(int [][] board){
+        return board[0].length;
+    }
     public void play(int howMany) {
     }
 }
-//coment
